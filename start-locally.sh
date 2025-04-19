7759685e4a0fbd3dc2c20e77cbc0074f259e6960
ruann3res <ruanpablo1218@gmail.com>
2025-04-19 17:09:10 -0300

    Adiciona shebang e permissão de execução

--- start-locally.sh
+++ start-locally.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 echo "Iniciando ambiente local via uaipy CLI"
-echo "Repositório: https://github.com/uaipy/scripts"
+echo "Repositório: https://github.com/ruann3res/script-test"
 echo "Data e hora: $(date) "
 echo "Usuário: $(whoami)"
